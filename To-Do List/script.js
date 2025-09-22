960ce06e54c2a34ed5f90e6a03fcef92ca4a580f
Sara M <msara12138@gmail.com>
2025-09-22 07:46:29 +0530

    Update script.js

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,6 +1,6 @@
 function addTask(){
     const task=document.getElementById("taskInput");
-    const taskText=task.inputMode.trim();
+    const taskText=task.value.trim();
 
     if(taskText !== ""){
         const ul=document.getElementById("todolist");
@@ -8,10 +8,17 @@ function addTask(){
         const span=document.createElement("span");
         span.textContent=taskText;
 
+        const editButton=document.createElement("button");
+        editButton.textContent="Edit";
+        editButton.onclick=()=>editTask(span);
 
+        const deleteButton=document.createElement("button");
+        deleteButton.textContent="Delete";
+        deleteButton.onclick=()=>removeTask(li);
+        
         li.appendChild(span);
         li.appendChild(editButton);
-        li.appendChild(removeButton);
+        li.appendChild(deleteButton);
 
         ul.appendChild(li);
         task.value = "";
